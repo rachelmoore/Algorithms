36938166d43af4cfec7a829df5b90763bf842ed8
Rachel Moore <rachel@rachelmoore.net>
2017-10-23 14:36:47 -0700

    Add BST practical solution

--- BST/lib/bst_practical.rb
+++ BST/lib/bst_practical.rb
@@ -1,3 +1,15 @@
 
 def kth_largest(tree_node, k)
+    k_largest = reverse_order_traversal(tree_node, [], k)
+    k_largest[k - 1]
 end
+
+
+def reverse_order_traversal(tree_node, arr, k)
+    return nil if tree_node.nil?
+    return arr if arr.length >= k 
+    reverse_order_traversal(tree_node.right, arr, k)
+    arr.push(tree_node)
+    reverse_order_traversal(tree_node.left, arr, k)
+    return arr 
+end 
